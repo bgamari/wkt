5e69278f5617f82e7ed86ffcd6cbef5d6e0fada8
Ben Gamari <bgamari.foss@gmail.com>
2013-09-16 11:42:45 -0400

    Types: Add instances

--- src/Data/Geo/WKT/Types.hs
+++ src/Data/Geo/WKT/Types.hs
@@ -1,7 +1,9 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TemplateHaskell, DeriveDataTypeable, DeriveGeneric #-}
 
 module Data.Geo.WKT.Types where
 
+import GHC.Generics
+import Data.Typeable
 import Control.Lens
 import Linear
 
@@ -10,17 +12,20 @@ import Linear
 data Parameter = Parameter { _paramName     :: String
                            , _paramValue    :: Double
                            }
+               deriving (Show, Eq, Ord, Typeable, Generic)
 makeLenses ''Parameter
 
 data Authority = Authority { _authorityName :: String
                            , _authorityCode :: String
                            }
+               deriving (Show, Eq, Ord, Typeable, Generic)
 makeLenses ''Authority
 
 data Unit = Unit { _unitName               :: String
                  , _unitConversionFactor   :: Double
                  , _unitAuthority          :: Maybe Authority
                  }
+          deriving (Show, Eq, Ord, Typeable, Generic)
 makeLenses ''Unit
 
 data Spheroid = Spheroid { _spheroidName                :: String
@@ -28,12 +33,14 @@ data Spheroid = Spheroid { _spheroidName                :: String
                          , _spheroidInverseFlattening   :: Double
                          , _spheroidAuthority           :: Maybe Authority
                          }
+              deriving (Show, Eq, Ord, Typeable, Generic)
 makeLenses ''Spheroid
 
 data ToWGS84 = ToWGS84 { _towgs84D      :: V3 Double
                        , _towgs84E      :: V3 Double
                        , _towgs84PPM    :: Double
                        }
+             deriving (Show, Eq, Ord, Typeable, Generic)
 makeLenses ''ToWGS84
 
 data Datum = Datum { _datumName         :: String
@@ -41,19 +48,23 @@ data Datum = Datum { _datumName         :: String
                    , _datumToWGS84      :: Maybe ToWGS84
                    , _datumAuthority    :: Maybe Authority
                    }
+           deriving (Show, Eq, Ord, Typeable, Generic)
 makeLenses ''Datum
 
 data PrimeMeridian = PrimeMeridian { _primemName      :: String
                                    , _primemLongitude :: Double
                                    , _primemAuthority :: Maybe Authority
                                    }
+                   deriving (Show, Eq, Ord, Typeable, Generic)
 makeLenses ''PrimeMeridian     
 
 data AxisDirection = North | South | East | West | Up | Down | Other
+                   deriving (Show, Eq, Ord, Typeable, Generic)
 
 data Axis = Axis { _axisName      :: String
                  , _axisDirection :: AxisDirection
                  }
+          deriving (Show, Eq, Ord, Typeable, Generic)
 makeLenses ''Axis     
 
 data GeographicCS = GeogCS { _geogcsName            :: String
@@ -63,11 +74,13 @@ data GeographicCS = GeogCS { _geogcsName            :: String
                            , _geogcsTwinAxes        :: Maybe (Axis, Axis)
                            , _geogcsAuthority       :: Maybe Authority
                            }
+                  deriving (Show, Eq, Ord, Typeable, Generic)
 makeLenses ''GeographicCS
 
 data Projection = Proj { _projName         :: String
                        , _projAuthority    :: Maybe Authority
                        }
+                deriving (Show, Eq, Ord, Typeable, Generic)
 makeLenses ''Projection
 
 data ProjectedCS = ProjCS { _projcsName             :: String
@@ -78,3 +91,5 @@ data ProjectedCS = ProjCS { _projcsName             :: String
                           , _projcsTwinAxes         :: Maybe (Axis, Axis)
                           , _projcsAuthority        :: Maybe Authority
                           }
+                 deriving (Show, Eq, Ord, Typeable, Generic)
+makeLenses ''ProjectedCS
